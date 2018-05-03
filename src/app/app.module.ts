0f59dc586367d639042e1ee134b77dd9d2db6aed
Vanya <slabko19@gmail.com>
2018-05-03 15:49:45 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsAreaComponent } from './components/main/news-area/news-area.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsAreaComponent
   ],
   imports: [
     BrowserModule,
