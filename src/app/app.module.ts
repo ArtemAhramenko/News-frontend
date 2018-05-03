8fada2705c164999250d61ac106c03bbde733163
ArtemAhramenko <artem.ahramenko98@gmail.com>
2018-05-03 14:52:26 +0300

    important

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AppRoutingModule} from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './components/main/header-component/header-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
