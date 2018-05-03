e2cdcac17cb504b6baf5b63332b73c453439eb9f
Vanya <slabko19@gmail.com>
2018-05-03 17:09:34 +0300

    create Arr for news. create Arr of titles

--- src/app/components/main/news-area/news-area.component.ts
+++ src/app/components/main/news-area/news-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-news-area',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-area.component.css']
 })
 export class NewsAreaComponent implements OnInit {
-
+  @Input() articleTitle;
   constructor() { }
 
   ngOnInit() {
