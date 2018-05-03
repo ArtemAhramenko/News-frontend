db3ad32f94ecb7b927b9ac622c544ee09f2d5290
ArtemAhramenko <artem.ahramenko98@gmail.com>
2018-05-03 16:36:34 +0300

    first step navbar and newsarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsAreaComponent } from './components/main/news-area/news-area.component';
-
+import {HeaderComponent} from "./components/main/header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewsAreaComponent
+    NewsAreaComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
