9743c848ad5724eb53077a6b2769ea2142988ba0
ArtemAhramenko <artem.ahramenko98@gmail.com>
2018-05-04 12:11:28 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/main/header/header.component';
+import {NewsAreaComponent} from "./components/main/news-area/news-area.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NewsAreaComponent
   ],
   imports: [
     BrowserModule,
