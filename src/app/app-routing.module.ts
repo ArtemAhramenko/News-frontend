a85aed3934b89e395cec8987c22f636970155aad
ArtemAhramenko <artem.ahramenko98@gmail.com>
2018-05-04 15:35:14 +0300

    add header(04.05.2018) refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HeaderComponent} from "./components/main/header/header.component";
+import { HeaderComponent } from "./components/main/header/header.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/', pathMatch: 'full'},
