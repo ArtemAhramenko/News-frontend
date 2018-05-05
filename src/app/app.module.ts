b20d850232e3ecf9c576bbc77d08f7441f1ce23b
ArtemAhramenko <artem.ahramenko98@gmail.com>
2018-05-05 16:37:12 +0300

    normal template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/main/header/header.component';
 import { CardComponent } from './components/main/card/card.component';
-
-
+import { LoginFormComponent } from "./components/main/login-form/login-form.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CardComponent
+    CardComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
