604a3f278921f1e428cc70abb6432dcfe02e1c9f
ArtemAhramenko <artem.ahramenko98@gmail.com>
2018-05-08 01:16:01 +0300

    header bug add model

--- src/app/components/auth/login-form/login-form.component.ts
+++ src/app/components/auth/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-login-form',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  registration: boolean = false;
+  userForm: FormGroup;
 
   constructor() { }
 
